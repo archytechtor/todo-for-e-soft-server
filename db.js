dedb9460655ab471db7e242c9944a7b58a3b4593
archytechtor <archytechtor@gmail.com>
2021-10-25 03:08:13 +0500

    and finally hope it`s starts :)

--- db.js
+++ db.js
@@ -1,18 +1,16 @@
 const {Sequelize} = require('sequelize')
 
-module.exports = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    options: {
-      dialect: 'postgres',
-      ssl: true,
-      dialectOptions: {
-        ssl: true
-      },
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT
+module.exports = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
   }
-)
+})
