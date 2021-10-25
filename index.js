3e58552af0f02991e1ab73eaeb58e38a8c481647
archytechtor <archytechtor@gmail.com>
2021-10-26 02:26:31 +0500

    finally

--- index.js
+++ index.js
@@ -13,14 +13,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
-
-if (process.env.NODE_ENV === 'production') {
-   app.use(express.static(path.join(__dirname, 'build')))
-   app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'build/index.html'))
-   })
-}
-
+app.use(express.static(path.join(__dirname, 'build')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'))
+})
 app.use(errorHandler)
 
 const start = async () => {
