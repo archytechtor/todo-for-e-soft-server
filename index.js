cce64c770210a8e1a7f70664093a00ce292a52d1
archytechtor <archytechtor@gmail.com>
2021-10-25 23:33:51 +0500

    fix form help text bug

--- index.js
+++ index.js
@@ -13,12 +13,14 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
+
 if (process.env.NODE_ENV === 'production') {
    app.use(express.static(path.join(__dirname, 'build')))
    app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, 'build/index.html'))
    })
 }
+
 app.use(errorHandler)
 
 const start = async () => {
